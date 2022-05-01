963ce02f4c64f86baf6df9735cf254de92bacbc7
Evgenii <example@gmail.com>
2022-05-01 21:37:37 +0300

    This first commit about training git

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// "use strict"; використовуєм строгий режим, який не дає можливості використовати старі методи коду
+"use strict";
+
+// a = 15;
+// console.log(a)
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+
+// const storeName = "Gravity";
+// const storeDiscription = {};
+// storeDiscription['budget'] = 10000;
+// storeDiscription['employees'] = ['Jack', 'Nick', 'Tony'];
+// storeDiscription['products'] = {
+//     aplle: 100,
+//     banana: 150
+// };
+// storeDiscription['open'] = true;
+
+// console.log(storeDiscription);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = [];
+answer[0] = prompt("What you firstname?", "");
+answer[1] = prompt("What yuo lastname?", "");
+answer[2] = prompt("How old are you?", "");
+
+document.write(answer);
